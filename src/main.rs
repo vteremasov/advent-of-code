f44abcf34d4f3e7fa86022b00dc2ad0628494b49
vteremasov <vteremasov@klika-tech.com>
2021-01-25 21:26:11 +0300

    Day 2 part 2

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::fs;
 use std::process;
 
 use year2019::day1::{calc_additional_fuel, calc_fuel, parse_input_day1};
-use year2019::day2::{computer_run, parse_input_day2, restore_state1202};
+use year2019::day2::{computer_run, computer_run_find, parse_input_day2, restore_state1202};
 
 pub mod year2019;
 
@@ -58,6 +58,13 @@ fn main() {
             println!(
                 "Result is: {:?}",
                 computer_run(restore_state1202(parse_input_day2(config.input)))
+            );
+            process::exit(0);
+        }
+        "day2_1" => {
+            println!(
+                "Result is: {}",
+                computer_run_find(restore_state1202(parse_input_day2(config.input)), 19690720)
             );
             process::exit(0);
         }
--- src/year2019/day2.rs
+++ src/year2019/day2.rs
@@ -23,21 +23,45 @@ pub fn calc(opcode: i64, v1: i64, v2: i64) -> i64 {
     }
 }
 
-pub fn computer_run(mut state: Vec<i64>) -> Vec<i64> {
+pub fn eloop(mut state: Vec<i64>) -> Vec<i64> {
     let size = state.len();
+    let mut ipointer = 0;
 
-    for idx in (0..size).step_by(4) {
-        if idx > size || state[idx] == 99 {
+    while ipointer < size {
+        if ipointer > size || state[ipointer] == 99 {
             return state;
         }
 
-        let to = state[idx + 3] as usize;
-        let v1_idx = state[idx + 1] as usize;
-        let v2_idx = state[idx + 2] as usize;
-        state[to] = calc(state[idx], state[v1_idx], state[v2_idx]);
+        let to = state[ipointer + 3] as usize;
+        let v1_idx = state[ipointer + 1] as usize;
+        let v2_idx = state[ipointer + 2] as usize;
+
+        state[to] = calc(state[ipointer], state[v1_idx], state[v2_idx]);
+
+        ipointer += 4;
     }
 
     state
+}
+
+pub fn computer_run(state: Vec<i64>) -> i64 {
+    eloop(state)[0]
+}
+
+pub fn computer_run_find(state: Vec<i64>, num: i64) -> i64 {
+    let mut s;
+    for verb in 0..100 {
+        for noun in 0..100 {
+            s = state.clone();
+            s[1] = verb;
+            s[2] = noun;
+            let result = computer_run(s);
+            if result == num {
+                return 100 * verb + noun;
+            }
+        }
+    }
+    0
 }
 
 pub fn restore_state1202(mut state: Vec<i64>) -> Vec<i64> {
@@ -51,25 +75,22 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_computer_run() {
+    fn test_eloop() {
         let input = vec![1, 0, 2, 1, 99, 1, 2, 3];
-        let res = computer_run(input);
+        let res = eloop(input);
         assert_eq!(res, vec![1, 3, 2, 1, 99, 1, 2, 3]);
     }
 
     #[test]
-    fn test_computer_run1() {
+    fn test_eloop1() {
         let input = vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50];
-        let res = computer_run(input);
+        let res = eloop(input);
         assert_eq!(res, vec![3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]);
-        assert_eq!(computer_run(vec![1, 0, 0, 0, 99]), vec![2, 0, 0, 0, 99]);
-        assert_eq!(computer_run(vec![2, 3, 0, 3, 99]), vec![2, 3, 0, 6, 99]);
+        assert_eq!(eloop(vec![1, 0, 0, 0, 99]), vec![2, 0, 0, 0, 99]);
+        assert_eq!(eloop(vec![2, 3, 0, 3, 99]), vec![2, 3, 0, 6, 99]);
+        assert_eq!(eloop(vec![2, 4, 4, 5, 99, 0]), vec![2, 4, 4, 5, 99, 9801]);
         assert_eq!(
-            computer_run(vec![2, 4, 4, 5, 99, 0]),
-            vec![2, 4, 4, 5, 99, 9801]
-        );
-        assert_eq!(
-            computer_run(vec![1, 1, 1, 4, 99, 5, 6, 0, 99]),
+            eloop(vec![1, 1, 1, 4, 99, 5, 6, 0, 99]),
             vec![30, 1, 1, 4, 2, 5, 6, 0, 99]
         );
     }
